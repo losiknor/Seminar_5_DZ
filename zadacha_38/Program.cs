e863ef4ffa142017fed734ca1d97cefc034108dd
Larina <Losiknor@gmail.com>
2022-02-17 20:08:43 +0700

    Решение задачи 38

--- zadacha_38/Program.cs
+++ zadacha_38/Program.cs
@@ -1,2 +1,15 @@
 ﻿// Задача 38: Найти сумму чисел одномерного массива, стоящих на нечётной позиции.
-Console.WriteLine("Hello, World!");
+int n = new Random().Next(3, 15);
+int[] array = new int[n];
+Random rnd1 = new Random();
+Console.Write("Задан массив: ");
+int sum_od_pos = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd1.Next(0, 999);
+    Console.Write(array[i] + " ");
+    if (i % 2 == 0)
+        sum_od_pos = sum_od_pos + array[i];
+}
+Console.WriteLine(" ");
+Console.Write("Сумма чисел, стоящих на нечётной позиции: " + sum_od_pos);
