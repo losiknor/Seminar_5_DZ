ea2e6cf598742fe78169a3f9476285b70dd86e6c
Larina <Losiknor@gmail.com>
2022-02-17 19:56:47 +0700

    Решение задачи 36

--- zadacha_36/Program.cs
+++ zadacha_36/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Задача 36: Задать массив, заполнить случайными положительными трёхзначными числами. 
 // Показать количество нечётных/чётных чисел.
-Console.WriteLine("Hello, World!");
+int n = new Random().Next(3, 15);
+int[] array = new int[n];
+Random rnd1 = new Random();
+Console.Write("Задан массив: ");
+int num_ev = 0;
+int num_od = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd1.Next(100, 999);
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+        num_ev = num_ev + 1;
+    else num_od = num_od + 1;
+}
+Console.WriteLine(" ");
+Console.Write("Четных чисел: " + num_ev);
+Console.Write("   Нечетных чисел: " + num_od);
